948e67adb2869e3992fd7099531d32b1d5d73877
Xinggang Li <lishinko@qq.com>
2014-06-20 10:47:46 +0800

    忘记提交了?

---
+++ output/struct.cpp
@@ -0,0 +1,11 @@
+#include"struct.cpp"
+void aStruct::anInner::parse(const char* name, const TiXmlElement& root){
+	::parse(aDouble, "aDouble",root);
+}
+void aStruct::parse(const char* name, const TiXmlElement& root){
+	::parse(a, "a",root);
+	::parse(b, "b",root);
+	::parse(anInnerInstance, "anInnerInstance",root);
+	::parse(v, "v",root);
+	::parse(v2, "v2",root);
+}
---
+++ output/struct.h
@@ -0,0 +1,17 @@
+    #ifndef _STRUCT_CPP_
+    #define _STRUCT_CPP_
+    struct aStruct 
+    {
+        int a; //这是一个注释
+        int b;/*另外一个注释*/
+        struct anInner
+        {
+            double aDouble;
+            void parse(const char* name, const TiXmlElement& root);
+        };
+        anInner anInnerInstance;
+        std::vector<int> v;
+        std::vector<map<int, double> > v2;
+        void parse(const char* name, const TiXmlElement& root);
+    };
+    ##endif
